b960433c85a728d1d1ba4b55cb9fb6d9c9760086
tsegaye abadi <tsegaye.abadi15@gmail.com>
2024-11-29 16:19:51 -0500

    file added

---
+++ finalproject/src/main/java/com/finalproject/restapi/LabTestResultRepository.java
@@ -0,0 +1,14 @@
+package com.finalproject.restapi;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+
+public interface LabTestResultRepository extends JpaRepository<LabTestResult, Long>{
+	List<LabTestResult> findByTestIdIn(List<Long> collect);
+
+	List<LabTestResult> findByTestId(Long testId);
+}
